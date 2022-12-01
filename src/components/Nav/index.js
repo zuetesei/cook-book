18a891c59cbe3b74aa4984f633bcaa14a1115d3d
zue <zuleikatesei@gmail.com>
2022-11-30 16:34:38 -0800

    added loggedInNav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,23 +2,6 @@ import React from "react";
 // import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav() {
-  const categories = [
-    // {
-    //   name: "commercial",
-    //   description:
-    //     "Photos of grocery stores, food trucks, and other commercial projects",
-    // },
-    // { name: "portraits", description: "Portraits of people in my life" },
-    // { name: "food", description: "Delicious delicacies" },
-    // {
-    //   name: "landscape",
-    //   description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-    // },
-  ];
-
-  // const handleClick = () => {
-  //   console.log("click handled");
-  // };
 
   return (
     <nav class="navbar navbar-expand-lg bg-light nav1">
