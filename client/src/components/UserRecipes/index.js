dab165427b67803c5466dbd1c4eabc05a7cf7a07
zue <zuleikatesei@gmail.com>
2022-12-01 20:12:15 -0800

    user recipes simple

---
+++ client/src/components/UserRecipes/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+
+function UserRecipes() {
+    return (
+        <div className='favRecipes'>
+            <h1> <MenuBookIcon /> My Recipes </h1>
+            <ul>
+                <li> Recipe 1 </li>
+                <li> Recipe 2 </li>
+                <li> Recipe 3 </li>
+                <li> Recipe 4 </li>
+            </ul>
+        </div>
+    )
+}
+
+export default UserRecipes;
