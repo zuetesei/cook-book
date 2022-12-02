5fafcffa53362216f5caab990996651eb7b4dadd
jonathon.biala@gmail.com <jonathon.biala@gmail.com>
2022-12-01 18:26:04 -0800

    recipes page added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import Auth from "./pages/Auth";
 import About from "./pages/About";
-import './styles/Auth.css';
-import './styles/Navbar.css';
-import './styles/Footer.css';
+import Recipes from "./pages/Recipes";
+import "./styles/Auth.css";
+import "./styles/Navbar.css";
+import "./styles/Footer.css";
 // import LoggedInNavbar from "./components/LoggedInNav";
 
 function App() {
@@ -18,12 +19,13 @@ function App() {
       <Router>
         <Nav />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='auth' element={<Auth />} />
-          <Route path='about' element={<About />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="auth" element={<Auth />} />
+          <Route path="about" element={<About />} />
+          <Route path="recipes" element={<Recipes />} />
         </Routes>
-        < Footer />
+        <Footer />
       </Router>
     </div>
   );
