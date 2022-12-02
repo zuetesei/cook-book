3760a7b09b4d9e73c256152cd36720d25560507a
Ryan <ryanpjhickey@gmail.com>
2022-12-01 20:46:08 -0800

    correct errors in code with naming conventions

--- server/controllers/reactionSchema-controller.js
+++ server/controllers/reactionSchema-controller.js
@@ -1,13 +1,13 @@
 const { reactionSchema, Recipe } = require('../models');
 
-const commentController = {
+const reactionSchemaController = {
   addreactionSchema({ params, body }, res) {
     console.log(params);
     reactionSchema.create(body)
       .then(({ _id }) => {
         return Recipe.findOneAndUpdate(
           { _id: params.recipeId },
-          { $push: { comments: _id } },
+          { $push: { reactionSchemas: _id } },
           { new: true }
         );
       })
@@ -22,10 +22,10 @@ const commentController = {
       .catch(err => res.json(err));
   },
 
-  // add reply to comment
+  // add reply to reactionSchema
   addReply({ params, body }, res) {
     reactionSchema.findOneAndUpdate(
-      { _id: params.commentId },
+      { _id: params.reactionSchemaId },
       { $push: { replies: body } },
       { new: true, runValidators: true }
     )
@@ -39,16 +39,16 @@ const commentController = {
       .catch(err => res.json(err));
   },
 
-  // remove comment
+  // remove reactionSchema
   removereactionSchema({ params }, res) {
-    reactionSchema.findOneAndDelete({ _id: params.commentId })
+    reactionSchema.findOneAndDelete({ _id: params.reactionSchemaId })
       .then(deletedreactionSchema => {
         if (!deletedreactionSchema) {
-          return res.status(404).json({ message: 'No comment with this id!' });
+          return res.status(404).json({ message: 'No reactionSchema with this id!' });
         }
         return Recipe.findOneAndUpdate(
           { _id: params.recipeId },
-          { $pull: { comments: params.commentId } },
+          { $pull: { reactionSchemas: params.reactionSchemaId } },
           { new: true }
         );
       })
@@ -64,7 +64,7 @@ const commentController = {
   // remove reply
   removeReply({ params }, res) {
     reactionSchema.findOneAndUpdate(
-      { _id: params.commentId },
+      { _id: params.reactionSchemaId },
       { $pull: { replies: { replyId: params.replyId } } },
       { new: true }
     )
@@ -73,4 +73,4 @@ const commentController = {
   }
 };
 
-module.exports = commentController;
+module.exports = reactionSchemaController;
