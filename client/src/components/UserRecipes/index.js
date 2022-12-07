a93d7f0bcf2b3a323692e2e0c94820352c14d4b2
jonathon.biala@gmail.com <jonathon.biala@gmail.com>
2022-12-07 09:57:58 -0800

    updates

--- client/src/components/UserRecipes/index.js
+++ client/src/components/UserRecipes/index.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import MenuBookIcon from '@mui/icons-material/MenuBook';
+import React from "react";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
 
 function UserRecipes() {
-    return (
-        <div className='favRecipes'>
-            <h1> <MenuBookIcon /> My Recipes </h1>
-            <ul>
-                <li> Recipe 1 </li>
-                <li> Recipe 2 </li>
-                <li> Recipe 3 </li>
-                <li> Recipe 4 </li>
-            </ul>
-        </div>
-    )
+  return (
+    <div className="favRecipes">
+      <h1>
+        {" "}
+        <MenuBookIcon /> My Recipes{" "}
+      </h1>
+      <ul>
+        <li> Recipe 1 </li>
+        <li> Recipe 2 </li>
+        <li> Recipe 3 </li>
+        <li> Recipe 4 </li>
+      </ul>
+    </div>
+  );
 }
 
 export default UserRecipes;
