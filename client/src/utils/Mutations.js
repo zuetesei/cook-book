365c31400405a7842f4b77cdcae36bfec92bafc4
ryanpjhickey <108383347+ryanpjhickey@users.noreply.github.com>
2022-12-06 20:10:16 -0800

    Update Mutations.js

--- client/src/utils/Mutations.js
+++ client/src/utils/Mutations.js
@@ -12,6 +12,7 @@ export const LOGIN_USER = gql`
   }
 `;
 
+
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
