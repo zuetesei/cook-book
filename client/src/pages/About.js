2d90a8b955ba8081c404dd21d328306fbdfaef1b
Huy <drac0123451@gmail.com>
2022-12-08 19:33:54 -0800

    changed

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import OnlyCooks from '../assets/onlycooks.png';
 import '../styles/About.css';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 function About() {
+    // object
+  const [donation] = useState({
+    name:"Donation",
+    price: 500,
+    description: 'Small donation'
+  })
+
+  async function handleToken(token, addresses) {
+    const response = await axios.post('http://localhost:3000/checkout', {token, donation})
+
+    // take response 
+    console.log(response.status)
+    let error, status
+    try {
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
     return (
         <div className='about-section'>
             <img className="only-cooks-logo" src={OnlyCooks} alt="Only Cooks logo"></img>
@@ -16,6 +38,24 @@ function About() {
             <a href="https://github.com/jonathonb1"> Jon </a>,
             <a href="https://github.com/ryanpjhickey"> Ryan </a> &
             <a href="https://github.com/zuetesei"> Zue Tesei </a>
+
+            <h2>Donations</h2>
+            <div className="Donate">
+      <div className="container">
+        <h1 className="text-center">Do you like what we do and would like to support? Please consider donating</h1>
+        <div className='form-group container'>
+          <StripeCheckout 
+          className='center'
+          stripeKey="pk_test_51MCegRHNHirOlOeXhAaOeJsW4Inb4cB6s7TPew0b5OA2jWJtZtvCqBdu36zp9VXuM7AdroPmbzE2tRl8swTzHBxO00AUuwpY78"
+          token={handleToken}
+          amount={donation.price}
+          name={donation.name}
+          billingAddress
+          shippingAddress
+          />
+        </div>
+      </div>
+    </div>
         </div>
     )
 }
